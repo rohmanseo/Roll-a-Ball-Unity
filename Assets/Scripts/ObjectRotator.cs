99a6e13d813cbd6c0f0ae74d111681222201913d
rohman <rohmann311@gmail.com>
2020-10-06 23:43:15 +0700

    init

---
+++ Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 movement = new Vector3(15, 30, 45);
+        transform.Rotate(movement * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody rigidBody;
+    private float movementX;
+    private float movementY;
+    private int score;
+    private int enemiesCollided;
+    private bool isGameOver;
+
+    public TextMeshProUGUI scoreText;
+    public GameObject winText;
+    public GameObject loseText;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        score = 0;
+        enemiesCollided = 0;
+        UpdateScore();
+        winText.SetActive(false);
+        loseText.SetActive(false);
+        isGameOver = false;
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        Vector2 movementVector = inputValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isGameOver)
+        {
+            bool isPickUp = other.gameObject.CompareTag("PickUp");
+            bool isEnemy = other.gameObject.CompareTag("Enemy");
+            if (isPickUp)
+            {
+                other.gameObject.SetActive(false);
+                score += 1;
+                UpdateScore();
+            }
+            else if (isEnemy)
+            {
+                other.gameObject.SetActive(false);
+                score -= 1;
+                enemiesCollided += 1;
+                UpdateScore();
+            }
+
+            if(score < 0)
+            {
+                LoseGame();
+            }
+
+            if (score == 10)
+            {
+                WinGame();
+            }
+            if (enemiesCollided == 3)
+            {
+                LoseGame();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX,0.0f,movementY);
+        rigidBody.AddForce(movement * speed);
+    }
+
+    void WinGame()
+    {
+        winText.SetActive(true);
+        isGameOver = true;
+    }
+
+    void LoseGame()
+    {
+        loseText.SetActive(true);
+        isGameOver = true;
+    }
+
+    void UpdateScore()
+    {
+           scoreText.text = "Score : " + score.ToString();
+    }
+
+}
